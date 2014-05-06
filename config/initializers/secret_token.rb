e58f1f0a6141f1673906c020539fc56a5d569525
Sanborn <sanbornh@rogers.com>
2014-05-06 15:38:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoiPolloiKitty::Application.config.secret_key_base = '798aff0ac9d588c151069b60d3a8754739ece0e5e337b794b51e7256e99844553ad837c1523fc7657da0bbee1fe384e6b0ab9c3d4dc52ddddd207f4d90aa2a64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HoiPolloiKitty::Application.config.session_store :cookie_store, key: '_hoi_polloi_kitty_session'
