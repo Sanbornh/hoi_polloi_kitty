7b8719a8613f9c2a569e63cee7bbbab363c34428
zap-rowsdower <brock.whitbread@gmail.com>
2014-05-08 16:42:23 -0400

    created user model and controller and views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506195623) do
+ActiveRecord::Schema.define(version: 20140508192749) do
 
   create_table "break_points", force: true do |t|
     t.integer  "value"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20140506195623) do
     t.integer  "goal"
     t.date     "funding_start"
     t.date     "funding_end"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
