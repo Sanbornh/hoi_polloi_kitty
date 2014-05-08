87c2a16c6d607bc47b113e34a39669d6834e1e7e
zap-rowsdower <brock.whitbread@gmail.com>
2014-05-08 16:49:07 -0400

    Integrate the ability to sign up and log in on the main page. Fix a bit of layout issues. Edit routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 HoiPolloiKitty::Application.routes.draw do
 
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  
   resources :break_points
   resources :projects
 
